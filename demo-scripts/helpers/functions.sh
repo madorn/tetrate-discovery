4f3ea0c3a6fd8374ea3cce462597516e08e5468d
Matt Dorn <madorn@gmail.com>
2022-09-28 13:58:22 -0500

    add longer sleep for cert manager op install

--- demo-scripts/helpers/functions.sh
+++ demo-scripts/helpers/functions.sh
@@ -232,9 +232,9 @@ function installCertManager() {
         kubectl apply --validate=false -f /tmp/ocp-cert-manager-og.yaml
         eval "echo \"$(cat "$ROOT"/templates/ocp-cert-manager-sub.yaml)\"" > /tmp/ocp-cert-manager-sub.yaml
         kubectl apply --validate=false -f /tmp/ocp-cert-manager-sub.yaml
-        sleep 10
+        sleep 20
         kubectl -n openshift-cert-manager-operator rollout status deployment/cert-manager-operator
-        sleep 5
+        sleep 10
         kubectl -n openshift-cert-manager rollout status deployment/cert-manager
         kubectl -n openshift-cert-manager create secret generic prod-route53-credentials-secret \
         --from-literal="secret-access-key=$ROUTE53_SECRET" 2>/dev/null
